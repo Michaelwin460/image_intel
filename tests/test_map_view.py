79c0b36ad9eda54651c445c4bb69d33af5534e98
Avroumiii <avroumi97@gmail.com>
2026-03-08 13:18:13 +0200

    resolve test map view

--- tests/test_map_view.py
+++ tests/test_map_view.py
@@ -1,4 +1,19 @@
 from map_view import create_map
 
 def test_create_map():
-    assert isinstance(create_map([]), str)
+    data = [
+        {
+            "filename": "test.jpg",
+            "latitude": 32.0853,
+            "longitude": 34.7818,
+            "has_gps": True,
+            "camera_make": "Test",
+            "camera_model": "Camera",
+            "datetime": "2025-01-01 10:00:00",
+        }
+    ]
+
+    html = create_map(data)
+
+    assert isinstance(html, str)
+    assert "test.jpg" in html
